0974064517fa035efbdd15c78fa2dcb239888a4e
EC2 Default User <ec2-user@ip-172-31-72-183.ec2.internal>
2021-02-12 11:08:01 +0000

    Initial Commit

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,9 +1,2 @@
 class List < ApplicationRecord
-    def change
-        create_table :lists do |t|
-            t.string :title
-            t.string :body
-            t.timestamps
-        end
-    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_10_125507) do
+ActiveRecord::Schema.define(version: 2021_02_11_151050) do
 
   create_table "lists", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
